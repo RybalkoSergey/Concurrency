8aad85a20ff05676b5693c8059beca730c752f34
Sergey Rybalko <s.rybalko@spd-ukraine.com>
2018-02-19 11:28:58 +0200

    Locks

--- src/test/java/TestSynchronized.java
+++ src/test/java/TestSynchronized.java
@@ -1,6 +1,9 @@
+import org.apache.commons.lang.time.StopWatch;
 import org.testng.annotations.Test;
 import utils.ConcurrentUtils;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 
 import static utils.ConcurrentUtils.sleep;
@@ -14,18 +17,16 @@ public class TestSynchronized {
     public void test1() {
         Counter counter = new Counter();
 
-        IntStream.range(0, 10000).forEach(i ->
-                new Thread(() -> {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        IntStream.range(0, 1000000)
+                .forEach(i -> executor.submit(() -> {
                     counter.inc1();
                     counter.inc2();
-                }).start()
-        );
+                }));
 
-        sleep(2);
+        ConcurrentUtils.stop(executor);
         System.out.println(counter.c1);
         System.out.println(counter.c2);
-
-
     }
 
     private class Counter {
@@ -37,13 +38,13 @@ public class TestSynchronized {
 
         public void inc1() {
             //synchronized(lock1) {
-                c1++;
+                c1 = c1 + 1;
             //}
         }
 
         public void inc2() {
             //synchronized(lock2) {
-                c2++;
+                c2 = c2 + 1;
             //}
         }
     }
