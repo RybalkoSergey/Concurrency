e16382b6430d015f83d6f9807c341d5d71bd6090
Sergey Rybalko <s.rybalko@spd-ukraine.com>
2018-02-20 11:11:41 +0200

    ReadWriteLocks

---
+++ src/test/java/TestStampedLock.java
@@ -0,0 +1,108 @@
+import org.testng.annotations.Test;
+import utils.ConcurrentUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.StampedLock;
+
+import static utils.ConcurrentUtils.sleep;
+
+/**
+ * Created by SRYBALKO on 2/20/2018.
+ */
+public class TestStampedLock {
+
+    @Test
+    public void test1() {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        Map<String, String> map = new HashMap<>();
+        StampedLock lock = new StampedLock();
+
+        executor.submit(() -> {
+            long stamp = lock.writeLock();
+            try {
+                sleep(1);
+                map.put("test", "test");
+            } finally {
+                lock.unlockWrite(stamp);
+            }
+        });
+
+        Runnable readTask = () -> {
+            long stamp = lock.readLock();
+            try {
+                System.out.println(map.get("test"));
+                sleep(1);
+            } finally {
+                lock.unlockRead(stamp);
+            }
+        };
+
+        executor.submit(readTask);
+        executor.submit(readTask);
+
+        ConcurrentUtils.stop(executor);
+    }
+
+    @Test
+    public void test2() {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        StampedLock lock = new StampedLock();
+
+        executor.submit(() -> {
+            long stamp = lock.tryOptimisticRead();
+            try {
+                System.out.println("Optimistic Lock Valid: " + lock.validate(stamp));
+                sleep(1);
+                System.out.println("Optimistic Lock Valid: " + lock.validate(stamp));
+                sleep(2);
+                System.out.println("Optimistic Lock Valid: " + lock.validate(stamp));
+                sleep(2);
+                System.out.println("Optimistic Lock Valid: " + lock.validate(stamp));
+            } finally {
+                lock.unlock(stamp);
+            }
+        });
+
+        executor.submit(() -> {
+            long stamp = lock.writeLock();
+            try {
+                System.out.println("Write Lock acquired");
+                sleep(2);
+            } finally {
+                lock.unlock(stamp);
+                System.out.println("Write done");
+            }
+        });
+
+        ConcurrentUtils.stop(executor);
+    }
+
+    @Test
+    public void test3() {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        StampedLock lock = new StampedLock();
+
+        executor.submit(() -> {
+            long count = 0;
+            long stamp = lock.readLock();
+            try {
+                if (count == 0) {
+                    stamp = lock.tryConvertToWriteLock(stamp);
+                    if (stamp == 0L) {
+                        System.out.println("Could not convert to write lock");
+                        stamp = lock.writeLock();
+                    }
+                    count = 23;
+                }
+                System.out.println(count);
+            } finally {
+                lock.unlock(stamp);
+            }
+        });
+
+        ConcurrentUtils.stop(executor);
+    }
+}
